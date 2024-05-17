ad976925291d06be9fbbf8ac82a91195d7777347
Davi Marques <davi.amqs@hotmail.com>
2024-05-16 23:31:45 -0300

    add(master): Adjust next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    }
+                ],
+            },
+        ];
+    }
+};
 
 export default nextConfig;

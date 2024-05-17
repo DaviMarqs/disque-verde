cd967c002d4bfe19484b3ef4b07eeba60534e7c6
Davi Marques <davi.amqs@hotmail.com>
2024-05-16 23:38:20 -0300

    test

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -1,7 +1,5 @@
-// import { dbConnect } from "@/lib/db";
-// import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   await dbConnect();
-//   return NextResponse.json({ message: "Server is Running!" });
-// }
+export async function GET() {
+  // await dbConnect();
+  // return NextResponse.json({ message: "Server is Running!" });
+}

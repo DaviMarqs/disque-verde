13d0d9d82a1069b8df4ac2ec7bf3238f794bbf87
Davi Marques <davi.amqs@hotmail.com>
2024-05-26 16:47:43 -0300

    fix(master): comment id route

--- src/app/api/ocurrence/[id]/route.ts
+++ src/app/api/ocurrence/[id]/route.ts
@@ -1,50 +1,50 @@
-import Occurrence from "@/app/models/Occurrence";
-import dbConnect from "@/lib/db";
-import { NextResponse } from "next/server";
+// import Occurrence from "@/app/models/Occurrence";
+// import dbConnect from "@/lib/db";
+// import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }) {
-  try {
-    await dbConnect();
-    const { _id } = params;
-    console.log("bateu aqui");
+// export async function GET(req: Request, { params }) {
+//   try {
+//     await dbConnect();
+//     const { _id } = params;
+//     console.log("bateu aqui");
 
-    const occurrence = await Occurrence.findById(_id);
-    if (!occurrence) {
-      return NextResponse.json(
-        { message: "Occurrence not found" },
-        { status: 404 }
-      );
-    }
+//     const occurrence = await Occurrence.findById(_id);
+//     if (!occurrence) {
+//       return NextResponse.json(
+//         { message: "Occurrence not found" },
+//         { status: 404 }
+//       );
+//     }
 
-    return NextResponse.json({ occurrence });
-  } catch (error) {
-    console.error("Error getting occurrence:", error);
-    return NextResponse.json(
-      { message: "Error getting occurrence", error },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ occurrence });
+//   } catch (error) {
+//     console.error("Error getting occurrence:", error);
+//     return NextResponse.json(
+//       { message: "Error getting occurrence", error },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export async function DELETE(req: Request, { params }) {
-  try {
-    await dbConnect();
-    const { _id } = params;
+// export async function DELETE(req: Request, { params }) {
+//   try {
+//     await dbConnect();
+//     const { _id } = params;
 
-    const occurrence = await Occurrence.findByIdAndDelete(_id);
-    if (!occurrence) {
-      return NextResponse.json(
-        { message: "Occurrence not found" },
-        { status: 404 }
-      );
-    }
+//     const occurrence = await Occurrence.findByIdAndDelete(_id);
+//     if (!occurrence) {
+//       return NextResponse.json(
+//         { message: "Occurrence not found" },
+//         { status: 404 }
+//       );
+//     }
 
-    return NextResponse.json({ message: "Occurrence deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting occurrence:", error);
-    return NextResponse.json(
-      { message: "Error deleting occurrence", error },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ message: "Occurrence deleted successfully" });
+//   } catch (error) {
+//     console.error("Error deleting occurrence:", error);
+//     return NextResponse.json(
+//       { message: "Error deleting occurrence", error },
+//       { status: 500 }
+//     );
+//   }
+// }

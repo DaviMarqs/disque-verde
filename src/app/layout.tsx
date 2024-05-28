0c9ef637a08ee90c52448b7266484b8c9f66d2d9
Davi Marques <davi.amqs@hotmail.com>
2024-05-28 15:29:29 -0300

    add(master): add base of the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/Header";
 import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+        <Header />
         {children}
         <Toaster />
       </body>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header className="bg-green-700 h-0 flex justify-center">
+      <div className="flex items-center mt-6 md:space-x-80">
+        {/* <img src="/disque_verde_logo.svg" alt="Logo Disque Verde" className="dark:invert" /> */}
+        <div>logo</div>
+        <h1 className="text-sm font-bold text-white font-medium">
+          Disque Verde
+        </h1>
+      </div>
+    </header>
+  );
+}

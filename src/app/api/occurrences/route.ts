d4ea985e245334d929db935ebbdab26899432efe
Davi Marques <davi.amqs@hotmail.com>
2024-05-30 15:11:52 -0300

    add(master): Add find one and delete route

--- src/app/api/occurrences/route.ts
+++ src/app/api/occurrences/route.ts
@@ -9,31 +9,34 @@ export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const page = parseInt(searchParams.get("page") || "1", 10);
     const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const id = searchParams.get("id"); // Novo parâmetro
+    console.log(id, "id");
 
-    const skip = (page - 1) * limit;
+    if (id) {
+      const occurrence = await Occurrence.findById(id);
+      if (!occurrence) {
+        return NextResponse.json(
+          { message: "Occurrence not found" },
+          { status: 404 }
+        );
+      }
+      return NextResponse.json({ occurrence });
+    } else {
+      const skip = (page - 1) * limit;
 
-    const totalOccurrences = await Occurrence.countDocuments({});
-    const occurrences = await Occurrence.find({})
-      .skip(skip)
-      .limit(limit)
-      .select([
-        "description",
-        "occurrence_type",
-        "occurrence_date",
-        "occurrence_time",
-        "status",
-        "createdAt",
-      ]);
+      const totalOccurrences = await Occurrence.countDocuments({});
+      const occurrences = await Occurrence.find({}).skip(skip).limit(limit);
 
-    return NextResponse.json({
-      occurrences,
-      pagination: {
-        page,
-        limit,
-        totalPages: Math.ceil(totalOccurrences / limit),
-        totalOccurrences,
-      },
-    });
+      return NextResponse.json({
+        occurrences,
+        pagination: {
+          page,
+          limit,
+          totalPages: Math.ceil(totalOccurrences / limit),
+          totalOccurrences,
+        },
+      });
+    }
   } catch (error) {
     console.error("Error getting occurrences:", error);
     return NextResponse.json(
@@ -42,7 +45,6 @@ export async function GET(req: Request) {
     );
   }
 }
-
 export async function POST(req: Request) {
   try {
     await dbConnect();
@@ -73,6 +75,42 @@ export async function POST(req: Request) {
     console.error("Error creating occurrence:", error);
     return NextResponse.json(
       { message: "Error creating occurrence", error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    await dbConnect();
+
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json(
+        { message: "Occurrence ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const deletedOccurrence = await Occurrence.findByIdAndDelete(id);
+
+    if (!deletedOccurrence) {
+      return NextResponse.json(
+        { message: "Occurrence not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Occurrence deleted successfully",
+      occurrence: deletedOccurrence,
+    });
+  } catch (error) {
+    console.error("Error deleting occurrence:", error);
+    return NextResponse.json(
+      { message: "Error deleting occurrence", error },
       { status: 500 }
     );
   }
